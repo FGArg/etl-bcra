a0195af3f15eb8ade634a80beeab847410b5a82c
FGA <FGA@facundos-air.lan>
2024-09-29 18:17:41 -0300

    airflow setup

---
+++ dags/etl_bcra/db_utils.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+default_connection_string = f'postgresql+psycopg2://airflow:airflow@postgres:5432/airflow'
+
+connection_string = os.getenv('SQL_ALCHEMY_CONN', default_connection_string)
+
+engine = create_engine(connection_string)
+
+
+def load_table(df, table_name):
+    df.to_sql(name=table_name, con=engine, if_exists='replace', index=False)
+    print(f"Data uploaded successfully to {table_name}!")
+    
+
+def read_table(table_name):
+    query = f"SELECT * FROM {table_name}"
+    df = pd.read_sql(query, con=engine)
+    return df
+    
---
+++ dags/etl_bcra_dag.py
@@ -0,0 +1,52 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from etl_bcra.extract_variables import extract_variables
+from etl_bcra.transform_variables import transform_variables
+from etl_bcra.extract_series import extract_series
+from etl_bcra.transform_series import transform_series
+from etl_bcra.db_utils import load_table, read_table 
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+def etl_variables():
+    variables_df = extract_variables()
+    if variables_df is not None:
+        variables_df = transform_variables(variables_df)
+        load_table(variables_df, "bcra_principales_variables")
+
+def etl_series():
+    variables_df = read_table("bcra_principales_variables")
+    if variables_df is not None:
+        series_df = extract_series(variables_df)
+        if series_df is not None:
+            series_df = transform_series(series_df)
+            load_table(series_df, "bcra_series")
+
+with DAG(
+    'etl_bcra_dag',
+    default_args=default_args,
+    description='ETL pipeline del BRCA',
+    schedule_interval='@daily',
+    catchup=False,
+    
+    ) as dag:
+
+    etl_variables_task = PythonOperator(
+        task_id='etl_variables',
+        python_callable=etl_variables
+    )
+
+    etl_series_task = PythonOperator(
+        task_id='etl_series',
+        python_callable=etl_series
+    )
+
+    etl_variables_task >> etl_series_task
