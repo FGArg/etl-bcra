707626154e1a80748b4d1f983f108372a38baa69
FGA <FGA@facundos-air.lan>
2024-10-14 11:15:09 -0300

    fixing blank likes for flake8 standards

--- tests/etl_bcra/test_transform_series.py
+++ tests/etl_bcra/test_transform_series.py
@@ -2,6 +2,7 @@ import pytest
 import pandas as pd
 from dags.etl_bcra.transform_series import transform_series
 
+
 @pytest.fixture
 def actual_series():
     return pd.DataFrame({
@@ -10,6 +11,7 @@ def actual_series():
         'tc_min':  [1000.0, 1001.0, 1002.0, 1003.0, 1004.0],
         'tasa_just': [900.0, 901.0, 902.0, 903.0, 904.0],
     })
+
 
 @pytest.fixture
 def new_series():
@@ -20,6 +22,7 @@ def new_series():
         'tasa_just': [910.0, 911.0, 912.0],
     })
 
+
 @pytest.fixture
 def expected_series():
     return pd.DataFrame({
@@ -28,6 +31,7 @@ def expected_series():
         'tc_min':  [1000.0, 1001.0, 1002.0, 1010.0, 1011.0, 1012.0],
         'tasa_just': [900.0, 901.0, 902.0, 910.0, 911.0, 912.0],
     })
+
 
 @pytest.fixture
 def new_series_with_duplicates():
@@ -38,15 +42,18 @@ def new_series_with_duplicates():
         'tasa_just': [910.0, 911.0, 912.0, 912.0],
     })
 
+
 def test_transform_series_with_none(new_series):
     df_updated = transform_series(None, new_series)
     expected = new_series.copy()
     pd.testing.assert_frame_equal(df_updated, expected)
 
+
 def test_transform_series_with_new_dates(actual_series, new_series, expected_series):
     df_updated = transform_series(actual_series, new_series)
     expected = expected_series.sort_values('fecha').reset_index(drop=True)
     pd.testing.assert_frame_equal(df_updated, expected)
+
 
 def test_transform_series_with_duplicates(actual_series, new_series_with_duplicates, expected_series):
     df_updated = transform_series(actual_series, new_series_with_duplicates)
