ce93e2285c6d00e777cb754903ea8d38b933d838
FGA <FGA@facundos-air.lan>
2024-10-17 00:00:59 -0300

    resources directory + short_code_mapping.json

--- dags/etl_bcra_dag.py
+++ dags/etl_bcra_dag.py
@@ -1,3 +1,5 @@
+import os
+import json
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
@@ -7,7 +9,9 @@ from etl_bcra.extract_series import extract_series
 from etl_bcra.transform_series import transform_series
 from etl_bcra.db_utils import load_table, read_table
 
-from sqlalchemy.exc import NoSuchTableError
+from sqlalchemy.exc import NoSuchTableError, ProgrammingError
+from psycopg2.errors import UndefinedTable
+
 
 default_args = {
     'owner': 'airflow',
@@ -20,8 +24,16 @@ default_args = {
 
 
 def etl_variables():
+    
+    dag_directory = os.path.dirname(os.path.abspath(__file__))
+    json_file_path = os.path.join(dag_directory, '../resources/short_code_mapping.json')
+    json_file_path = os.path.abspath(json_file_path)
+
+    with open(json_file_path, 'r') as file:
+        short_code_mapping = json.load(file)
+    
     variables_df = extract_variables()
-    variables_df = transform_variables(variables_df)
+    variables_df = transform_variables(variables_df, short_code_mapping)
     load_table(variables_df, "bcra_principales_variables")
 
 
@@ -31,8 +43,8 @@ def etl_series():
 
     try:
         actual_series_df = read_table("bcra_series")
-    except NoSuchTableError:
-        print("NoSuchTableError: bcra_series")
+    except (NoSuchTableError, ProgrammingError, UndefinedTable) as e:
+        print(f"Error with bcra_series: {e}")
         actual_series_df = None
 
     series_df = transform_series(actual_series_df, new_series_df)
